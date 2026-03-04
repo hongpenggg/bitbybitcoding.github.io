d639a1cab0906c4acad29a4acbba6441f03add60
Bit by Bit Coding <info.bbbcoding@gmail.com>
2026-03-04 07:38:02 +0000

    update to deploy with actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/bitbybitcoding.github.io/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
